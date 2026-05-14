36080307189bc4a9ec5256c99477c2e96d41ace4
Garima50 <garimagoyal705@gmail.com>
2026-05-14 10:43:31 +0530

    UC1-FEETINQUALITY COMMITTED

---
+++ QuantityMeasurementApp.java
@@ -0,0 +1,62 @@
+public class QuantityMeasurementApp {
+
+    // Inner class to represent Feet measurement
+    public static class Feet {
+
+        // Immutable value
+        private final double value;
+
+        // Constructor
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        /**
+         * Override equals() method to compare Feet objects
+         */
+        @Override
+        public boolean equals(Object obj) {
+
+            // Reference check
+            if (this == obj) {
+                return true;
+            }
+
+            // Null check
+            if (obj == null) {
+                return false;
+            }
+
+            // Type check
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+
+            // Safe casting
+            Feet other = (Feet) obj;
+
+            // Compare double values safely
+            return Double.compare(this.value, other.value) == 0;
+        }
+
+        /**
+         * Override hashCode() for consistency with equals()
+         */
+        @Override
+        public int hashCode() {
+            return Double.hashCode(value);
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        Feet value1 = new Feet(1.0);
+        Feet value2 = new Feet(1.0);
+
+        boolean result = value1.equals(value2);
+
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Output: Equal (" + result + ")");
+    }
+}
---
+++ QuantityMeasurementAppTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class QuantityMeasurementAppTest {
+
+    @Test
+    public void testFeetEquality_SameValue() {
+
+        QuantityMeasurementApp.Feet value1 =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        QuantityMeasurementApp.Feet value2 =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(value1.equals(value2));
+    }
+
+    @Test
+    public void testFeetEquality_DifferentValue() {
+
+        QuantityMeasurementApp.Feet value1 =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        QuantityMeasurementApp.Feet value2 =
+                new QuantityMeasurementApp.Feet(2.0);
+
+        assertFalse(value1.equals(value2));
+    }
+
+    @Test
+    public void testFeetEquality_NullComparison() {
+
+        QuantityMeasurementApp.Feet value1 =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        assertFalse(value1.equals(null));
+    }
+
+    @Test
+    public void testFeetEquality_DifferentClass() {
+
+        QuantityMeasurementApp.Feet value1 =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        Object obj = "1.0";
+        assertFalse(value1.equals(obj));
+    }
+
+    @Test
+    public void testFeetEquality_SameReference() {
+
+        QuantityMeasurementApp.Feet value1 =
+                new QuantityMeasurementApp.Feet(1.0);
+
+        assertTrue(value1.equals(value1));
+    }
+}
