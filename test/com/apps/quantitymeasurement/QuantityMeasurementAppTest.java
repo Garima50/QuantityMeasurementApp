8e1d031215c457f58cb4e3e27c4d6e804be1df14
Garima50 <garimagoyal705@gmail.com>
2026-05-18 15:43:33 +0530

    added feet class and tests

---
+++ test/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -0,0 +1,66 @@
+package com.apps.quantitymeasurement;
+
+import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// to run tests:
+// javac -cp "lib/*" *.java 
+// java -jar lib/junit-platform-console-standalone-1.10.2.jar --class-path . --scan-class-path
+
+public class QuantityMeasurementAppTest {
+//tests
+    @Test
+    public void testFeetEquality_SameValue() {
+
+        Feet value1 =
+                new Feet(1.0);
+
+        Feet value2 =
+                new Feet(1.0);
+
+        assertTrue(value1.equals(value2));
+    }
+
+    @Test
+    public void testFeetEquality_DifferentValue() {
+
+       Feet value1 =
+                new Feet(1.0);
+
+        Feet value2 =
+                new Feet(2.0);
+
+        assertFalse(value1.equals(value2));
+    }
+
+    @Test
+    public void testFeetEquality_NullComparison() {
+
+        Feet value1 =
+                new Feet(1.0);
+
+        assertFalse(value1.equals(null));
+    }
+
+    @Test
+    public void testFeetEquality_DifferentClass() {
+
+        Feet value1 =
+                new Feet(1.0);
+
+        Object obj = "1.0";
+        assertFalse(value1.equals(obj));
+    }
+
+    @Test
+    public void testFeetEquality_SameReference() {
+
+        Feet value1 =
+                new Feet(1.0);
+
+        assertTrue(value1.equals(value1));
+    }
+}
